cee6fd0e716b7c46d559d7f5a80ef2be42e2bc15
daniel-le97 <daniel97.le>
2022-12-01 00:21:01 -0700

    commit

--- liveChat.client/src/services/SubaseService.js
+++ liveChat.client/src/services/SubaseService.js
@@ -1,20 +1,19 @@
 import { AppState } from "../AppState";
 import { logger } from "../utils/Logger";
 import Pop from "../utils/Pop";
-// import { supabase } from "../utils/Supabase";
+import { supabase } from "../../supabase";
+
 import { chats } from "./FireBaseService";
-
-
-const supabase = useSupabase()
 
 class SupabaseService {
   async addSupabaseChat(chat) {
     try {
-      let data = {text: chat, createdAt: new Date(Date.now()) };
+      let data = { text: chat };
       // console.log(chat);
-      const res = await supabase.from("chats").upsert(data)
+      const res = await supabase.from("chats").upsert(data).select();
+      // const res = await supabase.from("chats")
       console.log(res.data, "data");
-      addOrSkipArray(AppState.chats, res.data[0]);
+      this.addOrSkipArray(AppState.supabase, res.data[0]);
     } catch (error) {
       Pop.error(error);
     }
@@ -23,21 +22,19 @@ class SupabaseService {
   async getChats() {
     try {
       const res = await supabase.from("chats").select();
-      AppState.chats = res.data;
+      AppState.supabase = res.data;
     } catch (error) {
       Pop.error(error);
     }
   }
 
-  
-}
-
-export const supabaseService = new SupabaseService();
-
-export function addOrSkipArray(arr, item) {
+  addOrSkipArray(arr, item) {
     let found = arr.find((i) => i.id == item.id);
     console.log(found);
     if (!found) {
       arr.push(item);
     }
   }
+}
+
+export const supabaseService = new SupabaseService();
