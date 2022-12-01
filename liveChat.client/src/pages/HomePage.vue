a1764aa29be3f19e6c814be7fd68007486737766
TungLe0319 <tung.le0319@gmail.com>
2022-12-01 00:23:44 -0700

    tung

--- liveChat.client/src/pages/HomePage.vue
+++ liveChat.client/src/pages/HomePage.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="container-fluid bg-dark">
     <div class="row gy-auto">
-      <div class="col-md-12 p-2  flip">
+      <div class="col-md-12 p-2 flip">
         <div class="d-flex">
           <p class="me-4 mb-0">Creator</p>
           <p class="mb-0">createdAt</p>
         </div>
         <div class="card p-4 bg-success rounded w-25">
-          <p class="fs-5">content</p>
+          <p class="fs-5" v-for="c in chats">{{ c }}</p>
         </div>
       </div>
-      <div class="col-md-12 fixed-bottom">
+      <div class="col-md-12">
         <form @submit.prevent="handleSubmit()" class="form-control">
-          <textarea name="" id="" cols="10" rows="10"> </textarea>
+          <div class="form-floating">
+            <textarea
+              class="form-control"
+              placeholder="Leave a comment here"
+              id="floatingTextarea2"
+              style="height: 100px"
+            ></textarea>
+            <label for="floatingTextarea2">Comments</label>
+          </div>
         </form>
       </div>
     </div>
@@ -22,13 +30,15 @@
 <script>
 import { computed } from "@vue/reactivity";
 import { AppState } from "../AppState.js";
+import { supabaseService } from "../services/SubaseService.js";
 
 export default {
   setup() {
     return {
-      chats:computed(()=> AppState.chats),
+      chats: computed(() => AppState.chats),
       async handleSubmit() {
         try {
+          await supabaseService.addSupabaseChat();
         } catch (error) {
           Pop.error(error, "[handleSubmit]");
         }
@@ -42,7 +52,8 @@ export default {
 .container-fluid {
   height: 100vh;
 }
-.flip div,p{
+.flip div,
+p {
   transform: rotate(180deg);
 }
 </style>
