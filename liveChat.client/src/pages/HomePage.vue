6f7f6d3f7c9d9cda1a5951c536b1665769d40e08
TungLe0319 <tung.le0319@gmail.com>
2022-12-01 00:07:55 -0700

    tung

--- liveChat.client/src/pages/HomePage.vue
+++ liveChat.client/src/pages/HomePage.vue
@@ -1,40 +1,28 @@
 <template>
   <div class="container-fluid bg-dark">
-    <div class="row gy-auto">
-      <div class="col-md-12 p-2  flip">
-        <div class="d-flex">
-          <p class="me-4 mb-0">Creator</p>
-          <p class="mb-0">createdAt</p>
-        </div>
-        <div class="card p-4 bg-success rounded w-25">
-          <p class="fs-5">content</p>
-        </div>
-      </div>
-      <div class="col-md-12 fixed-bottom">
-        <form @submit.prevent="handleSubmit()" class="form-control">
-          <textarea name="" id="" cols="10" rows="10"> </textarea>
-        </form>
-      </div>
-    </div>
+   <ChatComponent/>
   </div>
 </template>
 
 <script>
 import { computed } from "@vue/reactivity";
 import { AppState } from "../AppState.js";
+import ChatComponent from "../components/ChatComponent.vue";
 
 export default {
-  setup() {
-    return {
-      chats:computed(()=> AppState.chats),
-      async handleSubmit() {
-        try {
-        } catch (error) {
-          Pop.error(error, "[handleSubmit]");
-        }
-      },
-    };
-  },
+    setup() {
+        return {
+            chats: computed(() => AppState.chats),
+            async handleSubmit() {
+                try {
+                }
+                catch (error) {
+                    Pop.error(error, "[handleSubmit]");
+                }
+            },
+        };
+    },
+    components: { ChatComponent }
 };
 </script>
 
